a66ea09b2d25715173904d3869350ac291262ed3
Ankit Kumar <ankitk96969@gmail.com>
2024-03-08 18:02:05 +0530

    create a test api

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,7 @@
 import express from "express";
+import { test } from "../controllers/user.controller.js";
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.json({
-    message: "hello guys",
-  });
-});
+router.get("/test", test);;
 
 export default router;
